7c4371d2562deeed8471f6489d9c66f057d79835
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-07-21 14:08:10 -0500

    updated line 52 to correct exercises. improved formatting

--- where_exercises.sql
+++ where_exercises.sql
@@ -39,33 +39,35 @@ where first_name = 'Irena'
 -- 2. add a condition to the previous query to find everybody with those names who is also male
 select *
 from employees
-where last_name like 'E%'
-and gender = 'M';
+where (first_name = 'Irena'
+    or first_name = 'Vidya'
+    or first_name = 'Maya')
+    and gender ='M';
 
 -- 3. find all employees whose last name starts or ends with 'E'
 select *
 from employees
 where last_name like 'E%'
-or last_name like '%E';
+    or last_name like '%E';
 
 -- 4. duplicate the previous query and update it to find all employees whose last name starts
 -- and ends with 'E'
 select *
 from employees
 where last_name like 'E%'
-and last_name like '%E';
+    and last_name like '%E';
 
 -- 5. find all employees hired in the 90s and born on Christmas
 select *
 from employees
 where hire_date between '1990-01-01' and '1999-12-31'
-and birth_date like '%-12-25';
+    and birth_date like '%-12-25';
 
 -- 6. find all employees with a 'q' in their last name but not 'qu'
 select *
 from employees
 where last_name like '%q%'
-and not last_name like '%qu%';
+    and not last_name like '%qu%';
 
 
 
