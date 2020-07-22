a7586fe343556b1c33d327252b787cd0ef1bb8e5
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-07-22 10:35:58 -0500

    updated line 87

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -84,7 +84,7 @@ FROM salaries;
 -- and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, 
 -- the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
 SELECT CONCAT(LOWER(SUBSTR(first_name, 1, 1)), LOWER(SUBSTR(last_name, 1, 4)) 
-            ,'_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)) AS USErname 
+            ,'_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)) AS username 
             ,first_name 
             ,last_name
             ,birth_date
