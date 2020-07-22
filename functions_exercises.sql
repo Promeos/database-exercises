537fd9f5945fd6f880738a06b9c932c351f90598
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-07-22 10:34:11 -0500

    updated sql syntax to match conventional notation

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -1,92 +1,92 @@
--- use the employees database
-use employees;
+-- USE the employees database
+USE employees;
 
 -- find all employees with the first names 'Irena', 'Vidya', 'Maya'
--- order by first name
-select *
-from employees
-where first_name in ('Irena', 'Vidya', 'Maya')
-order by first_name;
+-- ORDER BY first name
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name;
 
 -- find all employees with the first names 'Irena', 'Vidya', 'Maya',
--- order by first_name, last_name
-select *
-from employees
-where first_name in ('Irena', 'Vidya', 'Maya')
-order by first_name, last_name;
+-- ORDER BY first_name, last_name
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
 
 -- find all employees with the first names 'Irena', 'Vidya', 'Maya',
--- order by last_name, first_name
-select *
-from employees
-where first_name in ('Irena', 'Vidya', 'Maya')
-order by last_name, first_name;
+-- ORDER BY last_name, first_name
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name, first_name;
 
 -- 2. find all employees whose last name starts with 'E'. sort by employee number
 -- [UPDATE] update your queries for employees whose names start and end with 'E'. 
--- use concat() to combine their first and last name together as a single column named full_name.
-select CONCAT(first_name, ' ', last_name) as full_name
-from employees
-where last_name like 'E%'
-order by emp_no;
+-- USE concat() to combine their first and last name together as a single column named full_name.
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no;
 
-select CONCAT(first_name, ' ', last_name) as full_name
-from employees
-where last_name like 'E%'
-    or last_name like '%E'
-    order by emp_no;
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%E'
+    ORDER BY emp_no;
 
-select CONCAT(first_name, ' ', last_name) as full_name
-from employees
-where last_name like 'E%'
-    and last_name like '%E'
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name LIKE '%E'
     group by emp_no;
 
 -- reverse the sort order for both queries (1-4, 5)
-select *
-from employees
-where first_name in ('Irena', 'Vidya', 'Maya')
-order by last_name desc, first_name desc;
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+ORDER BY last_name DESC, first_name DESC;
 
 -- [UPDATE] update your queries for employees whose names start and end with 'E'. 
--- use concat() to combine their first and last name together as a single column named full_name.
-select CONCAT(first_name, ' ', last_name) as full_name
-from employees
-where last_name like 'E%'
-order by emp_no desc;
+-- USE concat() to combine their first and last name together as a single column named full_name.
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
 
-select CONCAT(first_name, ' ', last_name) as full_name
-from employees
-where last_name like 'E%'
-    or last_name like '%E'
-    order by emp_no desc;
+SELECT CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    OR last_name LIKE '%E'
+    ORDER BY emp_no DESC;
 -- 3. [UPDATE] convert the names produced in your last query to all uppercase.
-select CONCAT(UPPER(first_name), ' ', UPPER(last_name)) as full_name
-from employees
-where last_name like 'E%'
-    and last_name like '%E'
-    group by emp_no desc;
+SELECT CONCAT(UPPER(first_name), ' ', UPPER(last_name)) AS full_name
+FROM employees
+WHERE last_name LIKE 'E%'
+    AND last_name LIKE '%E'
+    ORDER BY emp_no DESC;
 
 -- find all employees hired in the 90s and born on Christmas, sort by hire_date in descending order
 -- 4. For your query of employees born on Christmas and hired in the 90s
--- use datediff() to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE())
-select DATEDIFF(CURDATE(), hire_date) as length_of_employement
-from employees
-where hire_date between '1990-01-01' and '1999-12-31'
-    and birth_date like '%-12-25'
-    order by birth_date asc, hire_date desc;
+-- USE datediff() to find how many days they have been working at the company (Hint: You will also need to USE NOW() or CURDATE())
+SELECT DATEDIFF(CURDATE(), hire_date) AS length_of_employement
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+    AND birth_date LIKE '%-12-25'
+    ORDER BY birth_date ASC, hire_date DESC;
 
--- 5. find the smallest and largest salary from the salaries table.
-select MIN(salary) as smallest_salary, MAX(salary) as largest_salary
-from salaries;
+-- 5. find the smallest and largest salary FROM the salaries table.
+SELECT MIN(salary) AS smallest_salary, MAX(salary) AS largest_salary
+FROM salaries;
 
--- 6. Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, 
+-- 6. USE your knowledge of built in SQL functions to generate a USErname for all of the employees. A USErname should be all lowercase, 
 -- and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, 
 -- the month the employee was born, and the last two digits of the year that they were born. Below is an example of what the first 10 rows will look like:
 SELECT CONCAT(LOWER(SUBSTR(first_name, 1, 1)), LOWER(SUBSTR(last_name, 1, 4)) 
-            ,'_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)) as username 
+            ,'_', SUBSTR(birth_date, 6, 2), SUBSTR(birth_date, 3, 2)) AS USErname 
             ,first_name 
             ,last_name
             ,birth_date
-from employees;
+FROM employees;
 
