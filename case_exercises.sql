d1ee473b49a47e0b3217880042644e90b98ce564
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-08-02 10:11:08 -0500

    Adding updated file and removing misspelled file

--- case_exercises.sql
+++ case_exercises.sql
@@ -1,16 +1,18 @@
 use employees;
 
 
-select e.emp_no,
-        d_emp.dept_no,
-        e.hire_date as start_date,
-        d_emp.to_date as end_date,
-        case
-        when d_emp.to_date > now() then 1
-        else 0
-        end as is_current_employee
-from employees as e
-join dept_emp as d_emp using(emp_no);
+-- employee who has changed departments still appears in our query
+-- we must filter for the most recent department that the employee
+-- worked for, regardless of employment or seperation
+
+select dept_emp.emp_no,
+		max(hire_date) as start_date,
+		max(dept_emp.to_date) as end_date,
+		max(case when dept_emp.to_date > now() then 1 else 0 end)
+		as is_current_emp
+from dept_emp
+join employees using(emp_no)
+group by dept_emp.emp_no;
 -- where d_emp.to_date > now();
 
 
@@ -41,16 +43,12 @@ select concat(first_name, ' ', last_name) as fullname,
 from employees;
 
 -- How many employees were born in each decade?
-select decades, count(*) as total
-from
-(
-select first_name, last_name, birth_date,
+select count(*) as total,
 		case
-		when (year(birth_date) like '195%') then '50_decade'
-		when (year(birth_date) like '196%') then '60_decade'
+		when year(birth_date) like '195%' then '1950s'
+		when year(birth_date) like '196%' then '1960s'
 		end as decades
 from employees
-) as generations
 group by decades;
 
 -- What is the average salary for each of the following department groups:
