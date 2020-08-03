cf624b32321a5aa31511ee9ff152ddc687acf85d
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-08-02 19:32:38 -0500

    queried world database for country and life expectancy data for analysis

--- more_exercises.sql
+++ more_exercises.sql
@@ -64,5 +64,53 @@ from country
 group by Continent
 order by life_expectancy, Continent;
 
+-- What is the average life expectancy for each region, each continent? Sort the results from shortest to longest
+select region as Region, avg(LifeExpectancy) as life_expectancy
+from country
+group by region
+order by life_expectancy;
+
+select Continent, avg(LifeExpectancy) as life_expectancy
+from country
+group by Continent
+order by life_expectancy;
+
+-- Find all the countries whose local name is different from the official name
+select Name, LocalName
+from country
+where LocalName != NAME
+order by Name, LocalName;
+
+-- How many countries have a life expectancy less than x?
+select Region,
+		avg(LifeExpectancy) as Region_Life_Expectancy,
+		count(*) as Num_of_Countries_Below_Global_AVG_Life_Expectancy
+from country
+where LifeExpectancy < 
+(select avg(LifeExpectancy)
+from country)
+group by Region
+order by Num_of_Countries_Below_Global_AVG_Life_Expectancy;
 
 
+-- What state is city x located in?
+-- What region of the world is city x located in?
+-- What country (use the human readable name) city x located in?
+-- What is the life expectancy in city x?
+
+select city.Name as City,
+		city.District, country.Name as Country,
+		country.Region,
+		country.Continent,
+		LifeExpectancy as Life_Expectancy
+from city
+join country on country.code = city.CountryCode
+where city.Name = 'Resistencia';
+
+
+
+-- Extra
+select Region, avg(GNP), stddev(GNP), avg(LifeExpectancy), stddev(LifeExpectancy)
+from country
+where Continent = 'Africa'
+group by Region;
