ed52cc480f264a49bd15c2e0d5630781421beeca
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2020-08-10 02:57:36 -0500

    updated file

--- more_exercises.sql
+++ more_exercises.sql
@@ -114,3 +114,157 @@ select Region, avg(GNP), stddev(GNP), avg(LifeExpectancy), stddev(LifeExpectancy
 from country
 where Continent = 'Africa'
 group by Region;
+
+
+-- Sakila Database
+
+-- Display the first and last names in all lowercase of all the actors.
+select lower(first_name) as first_name, lower(last_name) as last_name
+from actor;
+
+-- You need to find the ID number, first name, and last name of an actor,
+-- of whom you know only the first name, "Joe." What is one query would you
+-- could use to obtain this information?
+select actor_id, first_name, last_name
+from actor
+where first_name = 'JOE';
+
+-- Find all actors whose last name contain the letters "gen":
+select *
+from actor
+where last_name like "%gen%";
+
+-- Find all actors whose last names contain the letters "li". 
+-- This time, order the rows by last name and first name, in that order.
+select *
+from actor
+where last_name like '%li%'
+order by last_name, first_name;
+
+-- Using IN, display the country_id and country columns for the following countries:
+-- Afghanistan, Bangladesh, and China
+select country_id, country
+from country
+where country in ('Afghanistan', 'Bangladesh', 'China');
+
+-- List the last names of all the actors, as well as how many actors have that last name.
+select last_name, count(last_name) as num_of_actors_with_last_name
+from actor
+group by last_name;
+
+-- List last names of actors and the number of actors who have that last name
+-- but only for names that are shared by at least two actors.
+select last_name, count(last_name) as num_of_actors_with_last_name
+from actor
+group by last_name
+having count(last_name) >= 2;
+
+-- You cannot locate the schema of the address table. Which query would you use to re-create it?
+show create table address;
+
+CREATE TABLE `address` (
+  `address_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `address` varchar(50) NOT NULL,
+  `address2` varchar(50) DEFAULT NULL,
+  `district` varchar(20) NOT NULL,
+  `city_id` smallint(5) unsigned NOT NULL,
+  `postal_code` varchar(10) DEFAULT NULL,
+  `phone` varchar(20) NOT NULL,
+  `location` geometry NOT NULL,
+  `last_update` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`address_id`),
+  KEY `idx_fk_city_id` (`city_id`),
+  SPATIAL KEY `idx_location` (`location`),
+  CONSTRAINT `fk_address_city` FOREIGN KEY (`city_id`) REFERENCES `city` (`city_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=606 DEFAULT CHARSET=utf8
+
+-- Use JOIN to display the first and last names, as well as the address, of each staff member.
+select first_name, last_name, address
+from staff
+join address using(address_id);
+
+-- Use JOIN to display the total amount rung up by each staff member in August of 2005
+select staff_id, concat(first_name, ' ', last_name) as staff_name, sum(amount) as august_sales
+from staff
+join payment using(staff_id)
+where payment_date like '2005-08-%'
+group by staff_id;
+
+-- List each film and the number of actors who are listed for that film.
+select title, count(film_id) as num_of_actors
+from film
+join film_actor using(film_id)
+group by title;
+
+-- How many copies of the film Hunchback Impossible exist in the inventory system?
+select title, count(film_id) as num_of_copies
+from film
+join inventory using(film_id)
+where title = 'Hunchback Impossible'
+group by title;
+
+-- The music of Queen and Kris Kristofferson have seen an unlikely resurgence.
+-- As an unintended consequence, films starting with the letters K and Q have also soared in popularity.
+-- Use subqueries to display the titles of movies starting with the letters K and Q whose language is English.
+select title as KQ_movie_titles
+from (
+select title
+from film
+join language using(language_id)
+where (title like 'K%'
+or title like 'Q%')
+and language_id = 1) as KQ_movies;
+
+-- Use subqueries to display all actors who appear in the film Alone Trip.
+select title, actor_id, first_name, last_name
+from (
+select title, actor_id, first_name, last_name
+from actor
+join film_actor using(actor_id)
+join film using(film_id)
+where title = 'Alone Trip') as alone_trip_actors;
+
+-- You want to run an email marketing campaign in Canada, for which you will need the names and 
+-- email addresses of all Canadian customers.
+select country, first_name, last_name, email
+from customer
+join address using(address_id)
+join city using(city_id)
+join country using(country_id)
+where country = 'Canada';
+
+-- Sales have been lagging among young families, and you wish to target all family movies for a promotion.
+-- Identify all movies categorized as famiy films.
+select *
+from film
+join film_category using(film_id)
+join category using(category_id)
+where name = 'Family';
+
+-- Write a query to display how much business, in dollars, each store brought in.
+select store_id, sum(amount) as sales
+from payment
+join staff using(staff_id)
+join store using(store_id)
+group by store_id;
+
+-- Write a query to display for each store its store ID, city, and country.
+select store_id, city, country
+from store
+join address using(address_id)
+join city using(city_id)
+join country using(country_id)
+
+
+-- List the top five genres in gross revenue in descending order. 
+-- (Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.)
+select name, sum(amount) as genre_sales
+from payment
+join rental using(rental_id)
+join inventory using(inventory_id)
+join film using(film_id)
+join film_category using(film_id)
+join category using(category_id)
+group by name
+order by genre_sales desc
+limit 5;
