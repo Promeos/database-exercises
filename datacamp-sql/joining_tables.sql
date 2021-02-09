b4ef745e167a2d1f4a6f47701c2f15dbf67517e6
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-02-08 20:41:00 -0600

    Completed SQL Server Fundamentals course

--- datacamp-sql/joining_tables.sql
+++ datacamp-sql/joining_tables.sql
@@ -210,3 +210,95 @@ FROM
 -- DECLARING VARIABLES
 -- DECLARE keyword creates a variable
 -- DECLARE THEN SET
+-- Declare the variable @region, and specify the data type of the variable
+DECLARE @region VARCHAR(10)
+
+-- Declare the variable @region
+DECLARE @region VARCHAR(10)
+-- Update the variable value
+SET @region = 'RFC'
+
+-- Declare the variable @region
+DECLARE @region VARCHAR(10)
+-- Update the variable value
+SET @region = 'RFC'
+
+SELECT description,
+       nerc_region,
+       demand_loss_mw,
+       affected_customers
+FROM grid
+WHERE nerc_region = @region;
+
+-- Declare @start
+DECLARE @start DATE
+-- SET @start to '2014-01-24'
+SET @start = '2014-01-24'
+
+-- Declare @start
+DECLARE @start DATE
+-- Declare @stop
+DECLARE @stop DATE
+
+-- SET @start to '2014-01-24'
+SET @start = '2014-01-24'
+-- SET @stop to '2014-07-02'
+SET @stop = '2014-07-02'
+
+
+-- Declare @start
+DECLARE @start DATE
+-- Declare @stop
+DECLARE @stop DATE
+-- Declare @affected
+DECLARE @affected INT
+
+-- SET @start to '2014-01-24'
+SET @start = '2014-01-24'
+-- SET @stop to '2014-07-02'
+SET @stop  = '2014-07-02'
+-- Set @affected to 5000
+SET @affected = 5000
+
+
+-- Declare your variables
+DECLARE @start DATE
+DECLARE @stop DATE
+DECLARE @affected INT;
+-- SET the relevant values for each variable
+SET @start = '2014-01-24'
+SET @stop  = '2014-07-02'
+SET @affected =  5000 ;
+
+SELECT 
+  description,
+  nerc_region,
+  demand_loss_mw,
+  affected_customers
+FROM 
+  grid
+-- Specify the date range of the event_date and the value for @affected
+WHERE event_date BETWEEN @start AND @stop
+AND affected_customers >= @affected;
+
+-- Create a temporary table and join tables onto it.
+-- Query the temp table
+SELECT  album.title AS album_title,
+  artist.name as artist,
+  MAX(track.milliseconds / (1000 * 60) % 60 ) AS max_track_length_mins
+-- Name the temp table #maxtracks
+INTO #maxtracks
+FROM album
+-- Join album to artist using artist_id
+INNER JOIN artist ON album.artist_id = artist.artist_id
+-- Join track to album using album_id
+INNER JOIN track ON track.album_id = album.album_id
+GROUP BY artist.artist_id, album.title, artist.name,album.album_id
+-- Run the final SELECT query to retrieve the results from the temporary table
+SELECT album_title, artist, max_track_length_mins
+FROM  #maxtracks
+ORDER BY max_track_length_mins DESC, artist;
+
+
+
+
