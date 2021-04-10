659a9f8f83cfe66091773529d413d08f861c5b77
promeos <christopher.logan.ortiz@gmail.com>
2021-04-09 22:52:05 -0500

    Updates

--- SQLZOO/select_basics.sql
+++ SQLZOO/select_basics.sql
@@ -138,3 +138,142 @@ SELECT name, population, area
 FROM world
 WHERE (area > 3000000 OR population > 250000000)
 AND NOT(area > 3000000 AND population > 250000000)
+
+-- 9. Show the name and population in millions and the GDP in billions for the countries of the continent 'South America'.
+-- Use the ROUND function to show the values to two decimal places.
+-- For South America show population in millions and GDP in billions both to 2 decimal places.
+-- Millions and billions
+SELECT name,
+    ROUND(population/1000000, 2),
+    ROUND(GDP/1000000000, 2)
+FROM world
+WHERE continent = 'South America'
+
+-- 10. Show the name and per-capita GDP for those countries with a GDP of at
+-- least one trillion (1000000000000; that is 12 zeros). Round this value to the nearest 1000.
+-- Show per-capita GDP for the trillion dollar countries to the nearest $1000.
+SELECT name,
+    ROUND(GDP/population, -3) AS GDP_per_capita
+FROM world
+WHERE GDP >= 1000000000000;
+
+-- 11. Greece has capital Athens.
+-- Each of the strings 'Greece', and 'Athens' has 6 characters.
+-- Show the name and capital where the name and the capital have the same number of characters.
+--     You can use the LENGTH function to find the number of characters in a string
+SELECT name,
+    capital
+FROM world
+WHERE LEN(name) = LEN(capital)
+
+
+-- 12. The capital of Sweden is Stockholm. Both words start with the letter 'S'.
+-- Show the name and the capital where the first letters of each match.
+-- Don't include countries where the name and the capital are the same word.
+
+--     You can use the function LEFT to isolate the first character.
+--     You can use <> as the NOT EQUALS operator.
+SELECT name,
+    capital
+FROM world
+WHERE LEFT(name, 1) = LEFT(capital, 1) AND (name <> capital);
+
+-- 13. Equatorial Guinea and Dominican Republic have all of the vowels (a e i o u) in the name.
+-- They don't count because they have more than one word in the name.
+-- Find the country that has all the vowels and no spaces in its name.
+-- You can use the phrase name NOT LIKE '%a%' to exclude characters from your results.
+-- The query shown misses countries like Bahamas and Belarus because they contain at least one 'a'.
+SELECT name
+FROM world
+WHERE name LIKE '%a%'
+    AND name LIKE '%e%'
+    AND name LIKE '%i%'
+    AND name LIKE '%o%'
+    AND name LIKE '%u%'
+    AND name NOT LIKE '% %';
+
+
+
+-- 1. Change the query shown so that it displays Nobel prizes for 1950.
+SELECT *
+FROM nobel
+WHERE yr = 1950;
+
+-- 2. Show who won the 1962 prize for Literature.
+SELECT winner
+FROM nobel
+WHERE subject = 'Literature'
+AND yr = 1962;
+
+-- 3. Show the year and subject that won 'Albert Einstein' his prize. 
+SELECT yr,
+subject
+FROM nobel
+WHERE winner = 'Albert Einstein';
+
+-- 4. Give the name of the 'Peace' winners since the year 2000, including 2000.
+SELECT winner
+FROM nobel
+WHERE yr >= 2000 AND subject = 'Peace';
+
+-- 5. Show all details (yr, subject, winner) of the Literature prize winners for 1980 to 1989 inclusive.
+SELECT *
+FROM nobel
+WHERE subject = 'Literature'
+AND yr BETWEEN 1980 AND 1989;
+
+-- 6. Show all details of the presidential winners:
+-- Theodore Roosevelt
+-- Woodrow Wilson
+-- Jimmy Carter
+-- Barack Obama
+SELECT *
+FROM nobel
+WHERE winner IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama')
+
+-- 7. Show the winners with first name John
+SELECT winner
+FROM nobel
+WHERE winner LIKE 'John %';
+
+-- 8. Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+SELECT *
+FROM nobel
+WHERE (subject = 'Physics' AND yr=1980) OR (subject='Chemistry' AND yr=1984);
+
+-- 9. Show the year, subject, and name of winners for 1980 excluding Chemistry and Medicine
+SELECT *
+FROM nobel
+WHERE yr=1980 AND subject NOT IN ('Chemistry', 'Medicine');
+
+-- 10. Show year, subject, and name of people who won a 'Medicine' prize in an early year
+-- (before 1910, not including 1910) together with winners of a 'Literature' prize in a later
+-- year (after 2004, including 2004)
+SELECT *
+FROM nobel
+WHERE (subject='Medicine' AND yr < 1910)
+OR (subject='Literature' AND yr >= 2004);
+
+-- 11. Find all details of the prize won by PETER GRÜNBERG 
+SELECT *
+FROM nobel
+WHERE winner LIKE 'PETER GRÜNBERG';
+
+-- 12. Find all details of the prize won by EUGENE O'NEILL 
+SELECT *
+FROM nobel
+WHERE winner = 'Eugene O''Neill';
+
+-- 13. List the winners, year and subject where the winner starts with Sir.
+-- Show the the most recent first, then by name order.
+SELECT winner, yr, subject
+FROM nobel
+WHERE winner LIKE 'Sir %'
+ORDER BY yr DESC, winner;
+
+-- 14. Show the 1984 winners and subject ordered by subject and winner name;
+-- but list Chemistry and Physics last.
+SELECT winner, subject
+FROM nobel
+WHERE yr=1984
+ORDER BY CASE WHEN subject IN ('Physics','Chemistry') THEN 1 ELSE 0 END, subject, winner;
