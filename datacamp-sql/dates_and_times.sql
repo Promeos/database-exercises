cce1299d3575b76f443218524b4a7115d349cd29
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-06-03 17:40:36 -0500

    Completed exercises using TRY_CONVERT, TRY_CAST, TRY_PARSE, compared safe conversion performance to non-safe conversion performance. Converted invalid dates to NULL values using safe conversion functions.

--- datacamp-sql/dates_and_times.sql
+++ datacamp-sql/dates_and_times.sql
@@ -78,7 +78,51 @@
         -- Lunar details
         -- ISO week of year
 
--- APPLY() 
+-- Time zone swaps with TODATETIMEOFFSET
+-- In order to switch time zones with a non UTC time, first add or subtract the number of hours from the current time zone
+-- to match UTC time you're converting to.
+-- After adding/ subtracting the hours to equal UTC, use the TODATETIMEOFFSET() function to append the UTC to the end of the datetime2 object.
+-- An example switching time zones:
+	-- DECLARE @SomeDate DATETIME2(3) = '2016-09-04 02:28:29.681'
+	-- SELECT TODATETIMEOFFSET(DATEADD(HOUR, 7, @SomeDate), '+02:00') AS BonnTime;
+
+-- Handling Invalid Dates
+-- In the real world, dates within our database may be invalid
+-- There are several "Safe Functions" to attempt datetime conversions:
+	-- TRY_CAST()
+	-- TRY_CONVERT()
+	-- TRY_PARSE()
+-- If an invalid date is passed to any of the functions above they will be converted to null.
+
+
+-- The following dates will parse correctly:
+-- SELECT
+	-- PARSE('01/08/2019' AS DATE USING 'en-us') AS Januray8US
+	-- PARSE('01/08/2019' AS DATE USING 'fr-fr') AS August1FR;
+
+	-- 2019-01-08 US locale
+	-- 2019-08-01 French locale
+
+
+-- The following dates will raise an ERROR:
+-- SELECT
+	-- PARSE('01/13/2019' AS DATE USING 'en-us') AS Januray13US,
+	-- PARSE('01/19/2019' AS DATE USING 'fr-fr') AS Smarch1FR;
+
+-- An error is raised when converting the date in French locale because there isn't a 13th month in the calendar.
+
+
+-- Safe date conversion: USE IT!
+-- SELECT
+	-- TRY_PARSE('01/13/2019' AS DATE USING 'en-us') AS Januray13US,
+	-- TRY_PARSE('01/13/2019' AS DATE USING 'fr-fr') AS Smarch1FR;
+
+-- The engine returns NULL for the date conversion to French locale.
+
+-- The cost of safety
+-- The safe functions have no decernable impact on performance compared to their non-safe counterparts.
+-- CONVERT() = TRY_CONVERT(), CAST() = TRY_CAST() performance speed.
+-- PARSE() = TRY_PARSE() also have comparable conversion speed an are still slower than CAST and CONVERT when converting strings to datetimes.
 
 ------------------------------ EXERCISES -----------------------------------------
 
@@ -333,3 +377,97 @@ WHERE
 	AND c.FiscalWeekOfYear > 30
     -- Only return weekends
 	AND c.isWeekend = 1;
+
+
+-- 8. Converting to a date offset
+DECLARE
+	@OlympicsClosingUTC DATETIME2(0) = '2016-08-21 23:00:00';
+
+SELECT
+	-- Fill in 7 hours back and a '-07:00' offset
+	TODATETIMEOFFSET(DATEADD(HOUR, -7, @OlympicsClosingUTC), '-07:00') AS PhoenixTime,
+	-- Fill in 12 hours forward and a '+12:00' offset.
+	TODATETIMEOFFSET(DATEADD(HOUR, 12, @OlympicsClosingUTC), '+12:00') AS TuvaluTime;
+
+
+-- 9. Try out type-safe date functions
+DECLARE
+	@GoodDateINTL NVARCHAR(30) = '2019-03-01 18:23:27.920',
+	@GoodDateDE NVARCHAR(30) = '13.4.2019',
+	@GoodDateUS NVARCHAR(30) = '4/13/2019',
+	@BadDate NVARCHAR(30) = N'SOME BAD DATE';
+
+-- The prior solution using TRY_CAST
+SELECT
+	TRY_CAST(@GoodDateINTL AS DATETIME2(3)) AS GoodDateINTL,
+	TRY_CAST(@GoodDateDE AS DATE) AS GoodDateDE,
+	TRY_CAST(@GoodDateUS AS DATE) AS GoodDateUS,
+	TRY_CAST(@BadDate AS DATETIME2(3)) AS BadDate;
+
+SELECT
+	TRY_PARSE(@GoodDateINTL AS DATETIME2(3)) AS GoodDateINTL,
+    -- Fill in the correct region based on our input
+    -- Be sure to match these data types with the
+    -- TRY_CAST() examples above!
+	TRY_PARSE(@GoodDateDE AS DATE USING 'de-de') AS GoodDateDE,
+	TRY_PARSE(@GoodDateUS AS DATE USING 'en-us') AS GoodDateUS,
+    -- TRY_PARSE can't fix completely invalid dates
+	TRY_PARSE(@BadDate AS DATETIME2(3) USING 'sk-sk') AS BadDate;
+
+
+-- 10. Convert imported data to dates with time zones
+WITH EventDates AS
+(
+    SELECT
+        -- Fill in the missing try-conversion function
+        TRY_CONVERT(DATETIME2(3), it.EventDate) AT TIME ZONE it.TimeZone AS EventDateOffset,
+        it.TimeZone
+    FROM dbo.ImportedTime it
+        INNER JOIN sys.time_zone_info tzi
+			ON it.TimeZone = tzi.name
+)
+SELECT
+    -- Fill in the approppriate event date to convert
+	CONVERT(NVARCHAR(50), ed.EventDateOffset) AS EventDateOffsetString,
+	CONVERT(DATETIME2(0), ed.EventDateOffset) AS EventDateLocal,
+	ed.TimeZone,
+    -- Convert from a DATETIMEOFFSET to DATETIME at UTC
+	CAST(ed.EventDateOffset AT TIME ZONE 'UTC' AS DATETIME2(0)) AS EventDateUTC,
+    -- Convert from a DATETIMEOFFSET to DATETIME with time zone
+	CAST(ed.EventDateOffset AT TIME ZONE 'US Eastern Standard Time'  AS DATETIME2(0)) AS EventDateUSEast
+FROM EventDates ed;
+
+
+-- 11. Test type-safe conversion function performance
+-- Try out how fast the TRY_CAST() function is
+-- by try-casting each DateText value to DATE
+DECLARE @StartTimeCast DATETIME2(7) = SYSUTCDATETIME();
+SELECT TRY_CAST(DateText AS DATE) AS TestDate FROM #DateText;
+DECLARE @EndTimeCast DATETIME2(7) = SYSUTCDATETIME();
+
+-- Determine how much time the conversion took by
+-- calculating the date difference from @StartTimeCast to @EndTimeCast
+SELECT
+    DATEDIFF(MILLISECOND, @StartTimeCast, @EndTimeCast) AS ExecutionTimeCast;
+
+-- Try out how fast the TRY_CONVERT() function is
+-- by try-converting each DateText value to DATE
+DECLARE @StartTimeConvert DATETIME2(7) = SYSUTCDATETIME();
+SELECT TRY_CONVERT(DATE, DateText) AS TestDate FROM #DateText;
+DECLARE @EndTimeConvert DATETIME2(7) = SYSUTCDATETIME();
+
+-- Determine how much time the conversion took by
+-- calculating the difference from start time to end time
+SELECT
+    DATEDIFF(MILLISECOND, @StartTimeConvert, @EndTimeConvert) AS ExecutionTimeConvert;
+
+-- Try out how fast the TRY_PARSE() function is
+-- by try-parsing each DateText value to DATE
+DECLARE @StartTimeParse DATETIME2(7) = SYSUTCDATETIME();
+SELECT TRY_PARSE(DateText AS DATE) AS TestDate FROM #DateText;
+DECLARE @EndTimeParse DATETIME2(7) = SYSUTCDATETIME();
+
+-- Determine how much time the conversion took by
+-- calculating the difference from start time to end time
+SELECT
+    DATEDIFF(MILLISECOND, @StartTimeParse, @EndTimeParse) AS ExecutionTimeParse;
