340aed732e934f2ba957c6247a2181b582fc4688
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-06-07 08:45:45 -0500

    Initialize sql

--- functions_for_manipulating_data_in_SQL_Server/strings.sql
+++ functions_for_manipulating_data_in_SQL_Server/strings.sql
@@ -1,10 +1,17 @@
-
--- Chapter 
+/*
+-- Chapter 3. Working with Strings
+Strings are one of the most commonly used data types in databases.
+It's important to know what you can do with them. In this chapter,
+you will learn how to manipulate strings, to get the results you want.
 
 ---------------------------------- NOTES -----------------------------------------
 
+FUNCTIONS FOR POSITIONS
 
 
+
+
+*/
 ------------------------------ EXERCISES -----------------------------------------
 
 -- 1. 
