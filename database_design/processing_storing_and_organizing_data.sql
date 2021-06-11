518f074e7ef5cea8d5d7b6a8c32c95aa1af91135
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-06-10 21:48:45 -0500

    Start file

--- database_design/processing_storing_and_organizing_data.sql
+++ database_design/processing_storing_and_organizing_data.sql
@@ -1,10 +1,12 @@
-
--- Chapter 
-
+/*
+-- Chapter 1
+Start your journey into database design by learning about the two approaches to data processing,
+OLTP and OLAP. In this first chapter, you'll also get familiar with the different forms data
+can be stored in and learn the basics of data modeling.
 -------------------------------- NOTES ------------------------------------------
 
 
-
+*/
 ------------------------------ EXERCISES -----------------------------------------
 
--- 1. 
+-- 1.
