1f26b2f9c046893ee3d9556fdf6be3c4c562b4ae
Christopher Logan Ortiz <christopher.logan.ortiz@gmail.com>
2021-07-02 15:05:23 -0500

    Begin EDA exercises using SQL.

--- writing_functions_and_stored_procedures/temporal_EDA.sql
+++ writing_functions_and_stored_procedures/temporal_EDA.sql
@@ -1,10 +1,66 @@
+/*
+Chapter 1. Temporal EDA, Variables & Date Manipulation
 
--- Chapter 
-
+Learn how to do effective exploratory data analysis on temporal data,
+create scalar and table variables to store data, and learn how to execute date manipulation.
+This chapter will also cover the following SQL functions: DATEDIFF( ), DATENAME( ), DATEPART( ),
+CAST( ), CONVERT( ), GETDATE( ), and DATEADD( ).
 -------------------------------- NOTES ------------------------------------------
 
+SQL FUNCTIONS FOR EDA
 
-
+*/
 ------------------------------ EXERCISES -----------------------------------------
 
--- 1. 
+-- 1. Transactions per day
+SELECT
+  -- Select the date portion of StartDate
+  CONVERT(DATE, StartDate) as StartDate,
+  -- Measure how many records exist for each StartDate
+  COUNT(*) as CountOfRows 
+FROM CapitalBikeShare 
+-- Group by the date portion of StartDate
+GROUP BY CONVERT(DATE, StartDate)
+-- Sort the results by the date portion of StartDate
+ORDER BY CONVERT(DATE, StartDate);
+
+
+-- 2. Seconds or no seconds?
+SELECT
+	-- Count the number of IDs
+	COUNT(ID) AS Count,
+    -- Use DATEPART() to evaluate the SECOND part of StartDate
+    "StartDate" = CASE WHEN DATEPART(SECOND,StartDate) = 0 THEN 'SECONDS = 0'
+					   WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
+FROM CapitalBikeShare
+GROUP BY
+    -- Use DATEPART() to Group By the CASE statement
+	CASE WHEN DATEPART(SECOND, StartDate) = 0 THEN 'SECONDS = 0'
+		 WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
+
+
+-- 3. Which day of week is busiest?
+SELECT
+    -- Select the day of week value for StartDate
+	DATENAME(WEEKDAY, StartDate) as DayOfWeek,
+    -- Calculate TotalTripHours
+	SUM(DATEDIFF(SECOND, StartDate, EndDate))/ 3600 as TotalTripHours 
+FROM CapitalBikeShare 
+-- Group by the day of week
+GROUP BY DATENAME(WEEKDAY, StartDate)
+-- Order TotalTripHours in descending order
+ORDER BY TotalTripHours DESC
+
+
+-- 4. Find the outliers
+SELECT
+	-- Calculate TotalRideHours using SUM() and DATEDIFF()
+  	SUM(DATEDIFF(SECOND, StartDate, EndDate))/ 3600 AS TotalRideHours,
+    -- Select the DATE portion of StartDate
+  	CONVERT(DATE, StartDate) AS DateOnly,
+    -- Select the WEEKDAY
+  	DATENAME(WEEKDAY, CONVERT(DATE, StartDate)) AS DayOfWeek 
+FROM CapitalBikeShare
+-- Only include Saturday
+WHERE DATENAME(WEEKDAY, StartDate) = 'Saturday' 
+GROUP BY CONVERT(DATE, StartDate);
